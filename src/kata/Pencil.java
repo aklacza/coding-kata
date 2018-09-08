6023f8285810aa7b2bfc02cc4cd81d87760c9c57
aklacza <andrewklacza@gmail.com>
2018-09-08 17:49:33 -0400

    Updated logic to check for spaces when decreasing point durability

--- src/kata/Pencil.java
+++ src/kata/Pencil.java
@@ -2,46 +2,51 @@
 //Parameters: length (how many times it may be sharpened)
 //pointDurability (measure of how much the point can write)
 //eraserDurability (how many characters the eraser can erase)
-
 package kata;
 
 public class Pencil {
+
     public int length;
     public int pointDurability;
     public int eraserDurability;
-    
-    public Pencil(int length, int pointDurability, int eraserDurability){
+
+    public Pencil(int length, int pointDurability, int eraserDurability) {
         this.length = length;
         this.pointDurability = pointDurability;
         this.eraserDurability = eraserDurability;
     }
-    
-    
+
     @Override
     public String toString() {
-        return "Pencil, " + "length: " + length 
-                + ", point durability: " + pointDurability 
+        return "Pencil, " + "length: " + length
+                + ", point durability: " + pointDurability
                 + ", eraser durability: " + eraserDurability;
     }
-    
-    public String write(String text){
+
+    public String write(String text) {
 
         //iterates through string to count number of capital letters
         //and lower case letters
         int countUpperCase = 0;
         int countLowerCase = 0;
-        
-        for(int x = 0; x<text.length(); x++){
-            if(Character.isUpperCase(text.charAt(x))){
-                countUpperCase++;
-            }
-            if(Character.isLowerCase(text.charAt(x))){
-                countLowerCase++;
+        int countSymbols = 0;
+
+        for (int x = 0; x < text.length(); x++) {
+            //first check to see if the character is a space
+            //then check to see if it's a capital
+            //if it's not a capital, it could be a number or a symbol, which also
+            //decrease one from pointDurability
+            if (!Character.isSpaceChar(text.charAt(x))) {
+                if (Character.isUpperCase(text.charAt(x))) {
+                    countUpperCase++;
+                } else {
+                    countLowerCase++;
+                }
+
             }
         }
-        this.pointDurability -= ((countUpperCase *2 ) + (countLowerCase));
+        this.pointDurability -= ((countUpperCase * 2) + (countLowerCase));
 
-        
         return text;
     }
 
@@ -68,8 +73,5 @@ public class Pencil {
     public void setEraserDurability(int eraserDurability) {
         this.eraserDurability = eraserDurability;
     }
-    
-    
+
 }
-
-
